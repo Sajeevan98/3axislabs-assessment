c4b7a9baefa68dd784b2e494beee694347c64a42
Sajeevan98 <sajee91098@gmail.com>
2025-07-01 13:34:48 +0530

    #1, project initiated with root folders

---
+++ server/src/main/java/com/attendance/system/loginservice/LoginServiceWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.system.loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiceWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiceWithJwtApplication.class, args);
+	}
+
+}
