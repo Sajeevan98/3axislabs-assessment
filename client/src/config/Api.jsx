db795777046c9c9912c5038a66a8051279903b0c
Sajeevan98 <sajee91098@gmail.com>
2025-07-01 17:33:14 +0530

    #3.5, created Api.jsx component in config folder

---
+++ client/src/config/Api.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const Api = axios.create({
+  baseURL: 'http://localhost:8082/',
+});
+
+Api.interceptors.request.use(config => {
+  const token = localStorage.getItem('jwtToken');
+  // console.log(token);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default Api; 
